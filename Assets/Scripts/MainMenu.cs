d4db0b90dae20c13ba42914816d7ce032ca5e522
dn004 <d.ntwari@alustudent.com>
2024-01-24 12:02:42 +0200

    colorblindess toggle done/ all done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	public Material trapMat;
+	public Material goalMat;
+	public Toggle colorblindMode;
     public void PlayMaze()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+		if (colorblindMode.isOn)
+    	{
+			trapMat.color = new Color32(255, 112, 0, 255);
+			goalMat.color = Color.blue;
+    	}
+
+		else if (!colorblindMode.isOn)
+		{
+			trapMat.color = new Color32(255, 0, 0, 255);
+        	goalMat.color = new Color32(0, 255, 0, 255);
+		}
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
 	public void QuitMaze()
